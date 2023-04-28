bd1cce4de35fe7eb16ca6537b20dc3dffd487adb
youssra <youssraadjou1@gmail.com>
2023-04-28 14:41:12 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,16 @@
 
 void more_numbers(void)
 {
-        int b, j;
+	int b, j;
 
-        for (b = 1; b <= 10; b++)
-        {
-                for (j = 0; j <= 14; j++)
-                {
-                        if (j >= 10)
-                                _putchar('1');
-                        _putchar (j % 10 + '0');
-                }
-                _putchar('\n');
-        }
+	for (b = 1; b <= 10; b++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
