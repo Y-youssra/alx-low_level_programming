a88280b067ecf1ba985f726a398d86329471e4f0
youssra <youssraadjou1@gmail.com>
2023-05-18 09:22:24 -0700

    task number 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,14 +5,14 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int _putchar(char b);
-void *malloc_checked(unsigned int a);
-char *string_nconcat(char *x1, char *x2, unsigned int m);
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
-int _strlen(char *x);
-int is_digit(char *x);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
